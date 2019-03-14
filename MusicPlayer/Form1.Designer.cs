37d2f6841764499e6f1f676485dbdb398ca2ba8d
Xavier Hodges <xh00125@georgiasouthern.edu>
2019-03-14 14:32:19 -0400

    test

--- MusicPlayer/Form1.Designer.cs
+++ MusicPlayer/Form1.Designer.cs
@@ -48,10 +48,9 @@
             // 
             // playBtn
             // 
-            this.playBtn.Location = new System.Drawing.Point(305, 548);
-            this.playBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.playBtn.Location = new System.Drawing.Point(229, 445);
             this.playBtn.Name = "playBtn";
-            this.playBtn.Size = new System.Drawing.Size(68, 41);
+            this.playBtn.Size = new System.Drawing.Size(51, 33);
             this.playBtn.TabIndex = 0;
             this.playBtn.Text = "Play";
             this.playBtn.UseVisualStyleBackColor = true;
@@ -59,50 +58,45 @@
             // 
             // pauseBtn
             // 
-            this.pauseBtn.Location = new System.Drawing.Point(431, 548);
-            this.pauseBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pauseBtn.Location = new System.Drawing.Point(323, 445);
             this.pauseBtn.Name = "pauseBtn";
-            this.pauseBtn.Size = new System.Drawing.Size(68, 41);
+            this.pauseBtn.Size = new System.Drawing.Size(51, 33);
             this.pauseBtn.TabIndex = 1;
             this.pauseBtn.Text = "Pause";
             this.pauseBtn.UseVisualStyleBackColor = true;
             // 
             // prevBtn
             // 
-            this.prevBtn.Location = new System.Drawing.Point(168, 548);
-            this.prevBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.prevBtn.Location = new System.Drawing.Point(126, 445);
             this.prevBtn.Name = "prevBtn";
-            this.prevBtn.Size = new System.Drawing.Size(76, 41);
+            this.prevBtn.Size = new System.Drawing.Size(57, 33);
             this.prevBtn.TabIndex = 2;
             this.prevBtn.Text = "Previous";
             this.prevBtn.UseVisualStyleBackColor = true;
             // 
             // nextBtn
             // 
-            this.nextBtn.Location = new System.Drawing.Point(552, 548);
-            this.nextBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nextBtn.Location = new System.Drawing.Point(414, 445);
             this.nextBtn.Name = "nextBtn";
-            this.nextBtn.Size = new System.Drawing.Size(76, 41);
+            this.nextBtn.Size = new System.Drawing.Size(57, 33);
             this.nextBtn.TabIndex = 3;
             this.nextBtn.Text = "Next";
             this.nextBtn.UseVisualStyleBackColor = true;
             // 
             // shuffleBtn
             // 
-            this.shuffleBtn.Location = new System.Drawing.Point(684, 548);
-            this.shuffleBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.shuffleBtn.Location = new System.Drawing.Point(513, 445);
             this.shuffleBtn.Name = "shuffleBtn";
-            this.shuffleBtn.Size = new System.Drawing.Size(76, 41);
+            this.shuffleBtn.Size = new System.Drawing.Size(57, 33);
             this.shuffleBtn.TabIndex = 4;
             this.shuffleBtn.Text = "Shuffle";
             this.shuffleBtn.UseVisualStyleBackColor = true;
             // 
             // repeatBtn
             // 
-            this.repeatBtn.Location = new System.Drawing.Point(825, 548);
-            this.repeatBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.repeatBtn.Location = new System.Drawing.Point(619, 445);
             this.repeatBtn.Name = "repeatBtn";
-            this.repeatBtn.Size = new System.Drawing.Size(76, 41);
+            this.repeatBtn.Size = new System.Drawing.Size(57, 33);
             this.repeatBtn.TabIndex = 5;
             this.repeatBtn.Text = "Repeat";
             this.repeatBtn.UseVisualStyleBackColor = true;
@@ -111,20 +105,18 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(403, 31);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(302, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(212, 39);
+            this.label1.Size = new System.Drawing.Size(169, 31);
             this.label1.TabIndex = 6;
             this.label1.Text = "Music Player";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(305, 87);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(229, 71);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(429, 334);
+            this.pictureBox1.Size = new System.Drawing.Size(322, 271);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
@@ -133,10 +125,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(387, 436);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(290, 354);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(251, 29);
+            this.label2.Size = new System.Drawing.Size(196, 24);
             this.label2.TabIndex = 8;
             this.label2.Text = "Dark Side of the Moon";
             // 
@@ -144,10 +135,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(459, 465);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(344, 378);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(113, 24);
+            this.label3.Size = new System.Drawing.Size(88, 18);
             this.label3.TabIndex = 9;
             this.label3.Text = "Def Leppard";
             // 
@@ -155,20 +145,18 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(427, 487);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(320, 396);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(176, 20);
+            this.label4.Size = new System.Drawing.Size(140, 16);
             this.label4.TabIndex = 10;
             this.label4.Text = "Dark Side of the Moon";
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(1048, 7);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Location = new System.Drawing.Point(786, 6);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(33, 30);
+            this.pictureBox2.Size = new System.Drawing.Size(25, 24);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 11;
             this.pictureBox2.TabStop = false;
@@ -177,16 +165,17 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(865, 137);
+            this.comboBox1.Location = new System.Drawing.Point(647, 98);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(153, 24);
+            this.comboBox1.Size = new System.Drawing.Size(116, 21);
             this.comboBox1.TabIndex = 12;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1092, 603);
+            this.ClientSize = new System.Drawing.Size(819, 490);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.label4);
@@ -201,7 +190,6 @@
             this.Controls.Add(this.pauseBtn);
             this.Controls.Add(this.playBtn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Music Player";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
