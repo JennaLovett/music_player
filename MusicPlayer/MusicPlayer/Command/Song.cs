f093ce16ddf4a5e9352a8428630c0484ac14da90
Jenna Lovett <jennablair12@gmail.com>
2019-03-21 17:49:18 -0400

    command pattern implemented

---
+++ MusicPlayer/MusicPlayer/Command/Song.cs
@@ -0,0 +1,38 @@
+﻿using AxWMPLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicPlayer.Command
+{
+    class Song
+    {
+        private string filePath;
+        private static AxWindowsMediaPlayer player1;
+
+        public Song(string file)
+        {
+            this.filePath = file;
+        }
+
+        public void play()
+        {
+            Form1.player.URL = this.filePath;
+            Form1.player.Ctlcontrols.play();
+        }
+
+        public void pause()
+        {
+            Form1.player.Ctlcontrols.pause();
+        }
+
+        public string getFilePath ()
+        {
+            return this.filePath;
+        }
+    }
+}
---
+++ MusicPlayer/MusicPlayer/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections;
+using MusicPlayer.Command;
+using AxWMPLib;
+
+namespace MusicPlayer
+{
+    public partial class Form1 : Form
+    {
+        ArrayList songs;
+        int currentSongPointer;
+        int slot;
+        public static AxWindowsMediaPlayer player;
+        CommandInvoker cmdControl;
+
+        public Form1()
+        {
+            InitializeComponent();
+            songs = new ArrayList();
+            currentSongPointer = -1;
+            player = new AxWindowsMediaPlayer();        //initializing the player that will play our music
+            player.CreateControl();     //allows us to control the player, like setting the URL
+            cmdControl = new CommandInvoker();
+            slot = 0;               //slot to be used when setting commands
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void playBtn_Click(object sender, EventArgs e)
+        {
+            cmdControl.playbtnPushed(currentSongPointer);
+        }
+
+        private void prevBtn_Click(object sender, EventArgs e)
+        {
+            if (currentSongPointer <= 0)
+            {
+                //do nothing
+            }
+            else
+            {
+                currentSongPointer--;
+                cmdControl.playbtnPushed(currentSongPointer);
+            }
+
+        }
+
+        private void importBtn_Click(object sender, EventArgs e)    //importing song file path
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();  //opens file viewer
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.filePath.Text = openFileDialog1.FileName;
+                player.URL = this.filePath.Text;
+                songs.Add(new Song(this.filePath.Text));
+                currentSongPointer++;
+                cmdControl.setCommand(slot, new PlayCommand((Song)songs[currentSongPointer]), new PauseCommand((Song)songs[currentSongPointer]));
+                slot++;
+            }
+
+        }
+
+        private void pauseBtn_Click(object sender, EventArgs e)
+        {
+            cmdControl.pausebtnPushed(currentSongPointer);
+        }
+
+        private void nextBtn_Click(object sender, EventArgs e)
+        {
+            if (currentSongPointer >= songs.Count)
+            {
+                //do nothing
+            }
+            else
+            {
+                currentSongPointer++;
+                cmdControl.playbtnPushed(currentSongPointer);
+            }
+
+        }
+    }
+}
