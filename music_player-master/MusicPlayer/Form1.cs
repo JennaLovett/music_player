8b3722db265fe2fc64608536bde2ba67eb148b9d
sm08504 <31252405+sm08504@users.noreply.github.com>
2019-03-26 14:42:31 -0400

    Add files via upload

---
+++ music_player-master/MusicPlayer/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections;
+using MusicPlayer.Command;
+using AxWMPLib;
+
+namespace MusicPlayer
+{
+    public partial class Form1 : Form
+    {
+        ArrayList songs;
+        int currentSongPointer;
+        int slot;
+        public static AxWindowsMediaPlayer player;
+        CommandInvoker cmdControl;
+
+        public Form1()
+        {
+            InitializeComponent();
+            songs = new ArrayList();
+            currentSongPointer = -1;
+            player = new AxWindowsMediaPlayer();        //initializing the player that will play our music
+            player.CreateControl();     //allows us to control the player, like setting the URL
+            cmdControl = new CommandInvoker();
+            slot = 0;               //slot to be used when setting commands
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void playBtn_Click(object sender, EventArgs e)
+        {
+            cmdControl.playbtnPushed(currentSongPointer);
+        }
+
+        private void prevBtn_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void importBtn_Click(object sender, EventArgs e)    //importing song file path
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();  //opens file viewer
+
+            if(openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.filePath.Text = openFileDialog1.FileName;      //places file path in textbox
+                player.URL = this.filePath.Text;                    //sets URL (file path) for the player
+                songs.Add(new Song(this.filePath.Text));            //creates new song object from this file path and adds it to list of songs
+            }
+        }
+
+        private void pauseBtn_Click(object sender, EventArgs e)
+        {
+            //implement today
+        }
+
+        private void nextBtn_Click(object sender, EventArgs e)
+        {
+            //implement today
+        }
+    }
+}
---
+++ music_player-master/MusicPlayer/MusicPlayer/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace MusicPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playBtn = new System.Windows.Forms.Button();
+            this.pauseBtn = new System.Windows.Forms.Button();
+            this.prevBtn = new System.Windows.Forms.Button();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.shuffleBtn = new System.Windows.Forms.Button();
+            this.repeatBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.songLabel = new System.Windows.Forms.Label();
+            this.artistLabel = new System.Windows.Forms.Label();
+            this.albumLabel = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.importBtn = new System.Windows.Forms.Button();
+            this.filePath = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playBtn
+            // 
+            this.playBtn.Location = new System.Drawing.Point(305, 548);
+            this.playBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.playBtn.Name = "playBtn";
+            this.playBtn.Size = new System.Drawing.Size(68, 41);
+            this.playBtn.TabIndex = 0;
+            this.playBtn.Text = "Play";
+            this.playBtn.UseVisualStyleBackColor = true;
+            this.playBtn.Click += new System.EventHandler(this.playBtn_Click);
+            // 
+            // pauseBtn
+            // 
+            this.pauseBtn.Location = new System.Drawing.Point(431, 548);
+            this.pauseBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.pauseBtn.Name = "pauseBtn";
+            this.pauseBtn.Size = new System.Drawing.Size(68, 41);
+            this.pauseBtn.TabIndex = 1;
+            this.pauseBtn.Text = "Pause";
+            this.pauseBtn.UseVisualStyleBackColor = true;
+            this.pauseBtn.Click += new System.EventHandler(this.pauseBtn_Click);
+            // 
+            // prevBtn
+            // 
+            this.prevBtn.Location = new System.Drawing.Point(168, 548);
+            this.prevBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.prevBtn.Name = "prevBtn";
+            this.prevBtn.Size = new System.Drawing.Size(76, 41);
+            this.prevBtn.TabIndex = 2;
+            this.prevBtn.Text = "Previous";
+            this.prevBtn.UseVisualStyleBackColor = true;
+            this.prevBtn.Click += new System.EventHandler(this.prevBtn_Click);
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Location = new System.Drawing.Point(552, 548);
+            this.nextBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(76, 41);
+            this.nextBtn.TabIndex = 3;
+            this.nextBtn.Text = "Next";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click);
+            // 
+            // shuffleBtn
+            // 
+            this.shuffleBtn.Location = new System.Drawing.Point(684, 548);
+            this.shuffleBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.shuffleBtn.Name = "shuffleBtn";
+            this.shuffleBtn.Size = new System.Drawing.Size(76, 41);
+            this.shuffleBtn.TabIndex = 4;
+            this.shuffleBtn.Text = "Shuffle";
+            this.shuffleBtn.UseVisualStyleBackColor = true;
+            // 
+            // repeatBtn
+            // 
+            this.repeatBtn.Location = new System.Drawing.Point(825, 548);
+            this.repeatBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.repeatBtn.Name = "repeatBtn";
+            this.repeatBtn.Size = new System.Drawing.Size(76, 41);
+            this.repeatBtn.TabIndex = 5;
+            this.repeatBtn.Text = "Repeat";
+            this.repeatBtn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(403, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 39);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Music Player";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(305, 87);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(429, 334);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // songLabel
+            // 
+            this.songLabel.AutoSize = true;
+            this.songLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.songLabel.Location = new System.Drawing.Point(446, 436);
+            this.songLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.songLabel.Name = "songLabel";
+            this.songLabel.Size = new System.Drawing.Size(141, 29);
+            this.songLabel.TabIndex = 8;
+            this.songLabel.Text = "Song Name";
+            // 
+            // artistLabel
+            // 
+            this.artistLabel.AutoSize = true;
+            this.artistLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.artistLabel.Location = new System.Drawing.Point(459, 465);
+            this.artistLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.artistLabel.Name = "artistLabel";
+            this.artistLabel.Size = new System.Drawing.Size(106, 24);
+            this.artistLabel.TabIndex = 9;
+            this.artistLabel.Text = "Artist Name";
+            // 
+            // albumLabel
+            // 
+            this.albumLabel.AutoSize = true;
+            this.albumLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.albumLabel.Location = new System.Drawing.Point(459, 489);
+            this.albumLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.albumLabel.Name = "albumLabel";
+            this.albumLabel.Size = new System.Drawing.Size(105, 20);
+            this.albumLabel.TabIndex = 10;
+            this.albumLabel.Text = "Album Name";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1048, 7);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(33, 30);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // importBtn
+            // 
+            this.importBtn.Location = new System.Drawing.Point(764, 159);
+            this.importBtn.Name = "importBtn";
+            this.importBtn.Size = new System.Drawing.Size(96, 40);
+            this.importBtn.TabIndex = 12;
+            this.importBtn.Text = "Import Song";
+            this.importBtn.UseVisualStyleBackColor = true;
+            this.importBtn.Click += new System.EventHandler(this.importBtn_Click);
+            // 
+            // filePath
+            // 
+            this.filePath.Location = new System.Drawing.Point(764, 219);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(225, 22);
+            this.filePath.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1092, 603);
+            this.Controls.Add(this.filePath);
+            this.Controls.Add(this.importBtn);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.albumLabel);
+            this.Controls.Add(this.artistLabel);
+            this.Controls.Add(this.songLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.repeatBtn);
+            this.Controls.Add(this.shuffleBtn);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.prevBtn);
+            this.Controls.Add(this.pauseBtn);
+            this.Controls.Add(this.playBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Music Player";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playBtn;
+        private System.Windows.Forms.Button pauseBtn;
+        private System.Windows.Forms.Button prevBtn;
+        private System.Windows.Forms.Button nextBtn;
+        private System.Windows.Forms.Button shuffleBtn;
+        private System.Windows.Forms.Button repeatBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label songLabel;
+        private System.Windows.Forms.Label artistLabel;
+        private System.Windows.Forms.Label albumLabel;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button importBtn;
+        private System.Windows.Forms.TextBox filePath;
+    }
+}
+
